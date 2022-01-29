e21553956c62fe694d87460ae5163d8217637f56
SACHU ABHIRAM <djkmkgamer57@gmail.com>
2022-01-29 10:35:23 +0530

    Update StringSession.js

--- WhiteDevil/StringSession.js
+++ WhiteDevil/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'WHITE;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'RDXQR;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
